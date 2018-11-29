ea75e3ed216de6cc94981d401eb198740193ded7
Nathan Vegdahl <cessen@cessen.com>
2018-11-29 11:09:48 -0800

    Added benchmarks for both Trifloat and Oct32 encoding/decoding.

---
+++ sub_crates/oct32norm/benches/bench.rs
@@ -0,0 +1,37 @@
+#[macro_use]
+extern crate bencher;
+extern crate oct32norm;
+extern crate rand;
+
+use bencher::{black_box, Bencher};
+use oct32norm::{decode, encode};
+use rand::{rngs::SmallRng, FromEntropy, Rng};
+
+//----
+
+fn encode_100_values(bench: &mut Bencher) {
+    let mut rng = SmallRng::from_entropy();
+    bench.iter(|| {
+        let x = rng.gen::<f32>() - 0.5;
+        let y = rng.gen::<f32>() - 0.5;
+        let z = rng.gen::<f32>() - 0.5;
+        for _ in 0..100 {
+            black_box(encode(black_box((x, y, z))));
+        }
+    });
+}
+
+fn decode_100_values(bench: &mut Bencher) {
+    let mut rng = SmallRng::from_entropy();
+    bench.iter(|| {
+        let v = rng.gen::<u32>();
+        for _ in 0..100 {
+            black_box(decode(black_box(v)));
+        }
+    });
+}
+
+//----
+
+benchmark_group!(benches, encode_100_values, decode_100_values,);
+benchmark_main!(benches);
---
+++ sub_crates/trifloat/benches/bench.rs
@@ -0,0 +1,37 @@
+#[macro_use]
+extern crate bencher;
+extern crate rand;
+extern crate trifloat;
+
+use bencher::{black_box, Bencher};
+use rand::{rngs::SmallRng, FromEntropy, Rng};
+use trifloat::{decode, encode};
+
+//----
+
+fn encode_100_values(bench: &mut Bencher) {
+    let mut rng = SmallRng::from_entropy();
+    bench.iter(|| {
+        let x = rng.gen::<f32>() - 0.5;
+        let y = rng.gen::<f32>() - 0.5;
+        let z = rng.gen::<f32>() - 0.5;
+        for _ in 0..100 {
+            black_box(encode(black_box((x, y, z))));
+        }
+    });
+}
+
+fn decode_100_values(bench: &mut Bencher) {
+    let mut rng = SmallRng::from_entropy();
+    bench.iter(|| {
+        let v = rng.gen::<u32>();
+        for _ in 0..100 {
+            black_box(decode(black_box(v)));
+        }
+    });
+}
+
+//----
+
+benchmark_group!(benches, encode_100_values, decode_100_values,);
+benchmark_main!(benches);
